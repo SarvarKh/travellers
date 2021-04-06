30459f33956d74094b55018ed321b3a56d5c8a68
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-04-07 01:03:01 +0500

    Add form to create user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        @user.save!
+        redirect_to users_path
+    end
+
+    private
+    def user_params
+        params.require(:user).
+            permit(:username, :full_name, :photo, :cover_image)
+    end
+end
