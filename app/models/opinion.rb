b8b9e458453433b698b7da172d0e8459d44120e5
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-04-07 00:25:28 +0500

    Add user and opinion model associations

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,4 +1,6 @@
 class Opinion < ApplicationRecord
     validates :author_id, presence: true
     validates :text, presence: true
+
+    belongs_to :author, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
     validates :full_name, presence: true
     validates :photo, presence: true
     validates :cover_image, presence: true
+
+    has_many :opinions, class_name: "Opinion", foreign_key: "author_id"
 end
