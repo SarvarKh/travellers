5285130f19d3a8494558a65da86d22936f30d95c
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-04-08 13:24:03 +0500

    Refactor current_user moving it to app.controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,6 @@ class UsersController < ApplicationController
     def index
         # flash.now[:notice] = "Logged in successfully"
         # flash.now[:alert] = "Invalid email"
-        if session[:user_id]
-            @user = User.find_by(id: session[:user_id]) 
-        end
         @users = User.all
     end
 
