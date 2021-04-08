735e52e23b254f76f722d130943728d9a590f700
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-04-09 01:00:19 +0500

    Add following associations

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,4 +1,6 @@
 class Following < ApplicationRecord
     validates :follower_id, presence: true
     validates :followed_id, presence: true
+
+    belongs_to :follower, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
     validates :cover_image, presence: true
 
     has_many :opinions, class_name: "Opinion", foreign_key: "author_id"
+    has_many :followings, class_name: "Following", foreign_key: "follower_id"
 end
