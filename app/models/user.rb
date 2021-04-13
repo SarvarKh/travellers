311fdb802d4f3e2feab06cfa622d6d2bda053a3b
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-04-13 14:01:08 +0500

    Add unit and integration tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :full_name, presence: true
-  validates :photo, presence: true
-  validates :cover_image, presence: true
 
   has_one_attached :photo
   has_one_attached :cover_image
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'with name Marcel' do
+    it 'follows user with name Gary' do
+      user1 = User.create(username: 'Marcel', full_name: 'Marcel Krause')
+      user2 = User.create(username: 'Gary', full_name: 'Gary Thomas')
+
+      follow = user1.followings.create!(follower_id: user1.id, followed_id: user2.id)
+
+      expect(User.find(user1.followings.first.followed_id).username).to eq("Gary")
+    end
+  end
+end
