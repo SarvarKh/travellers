96c3f99c317daa07545b33158548904990d5d5ef
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-04-16 20:21:09 +0500

    Configure cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :full_name, presence: true
+  # validates :photo, content_type: ['image/png', 'image/jpg', 'image/jpeg']
+  # validates :cover_image, content_type: ['image/png', 'image/jpg', 'image/jpeg']
 
-  has_one_attached :photo
-  has_one_attached :cover_image
+  has_one_attached :photo, dependent: :destroy
+  has_one_attached :cover_image, dependent: :destroy
 
-  has_many :opinions, class_name: 'Opinion', foreign_key: 'author_id'
-  has_many :followings, class_name: 'Following', foreign_key: 'follower_id'
-  has_many :followers, class_name: 'Following', foreign_key: 'followed_id'
-  has_many :votes
+  has_many :opinions, class_name: 'Opinion', foreign_key: 'author_id', dependent: :destroy
+  has_many :followings, class_name: 'Following', foreign_key: 'follower_id', dependent: :destroy
+  has_many :followers, class_name: 'Following', foreign_key: 'followed_id', dependent: :destroy
+  has_many :votes, dependent: :destroy
 end
