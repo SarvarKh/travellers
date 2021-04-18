199da664dba0e1dce37305f1b414364f9398124a
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-04-18 23:26:52 +0500

    Fix rubocop errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,83 +38,77 @@ module ApplicationHelper
   end
 
   def left_user_photo
-    if Current.user.photo.attached?
-      tag("img", src: url_for(Current.user.photo), class: ["img-80", "rounded"])
-    end
+    tag('img', src: url_for(Current.user.photo), class: %w[img-80 rounded]) if Current.user.photo.attached?
   end
 
   def login_navbar
-          if Current.user
-            content_tag :ul, :class=>"navbar-nav text-black-50" do
-              concat(content_tag(:li, class: "nav-item pe-2") do
-                link_to(edit_user_path(Current.user.id), class: "btn p-2") do
-                    concat(content_tag(:i, " ", class: "fas fa-user-edit"))
-                end
-              end)
-              concat(content_tag(:li, class: "nav-item pe-2") do
-                link_to(user_path(Current.user.id), class: "btn p-2") do
-                    concat(content_tag(:i, " ", class: "fas fa-user"))
-                end
-              end)
-              concat(content_tag(:li, class: "nav-item pe-2") do
-                link_to(logout_path, method: :delete, class: "btn p-2") do
-                  concat(content_tag(:span, "Log out"))
-                  concat(content_tag(:i, " ", class: "fas fa-door-open"))
-                end
-              end)
-            end
-          end
+    return unless Current.user
+
+    content_tag :ul, class: 'navbar-nav text-black-50' do
+      concat(content_tag(:li, class: 'nav-item pe-2') do
+        link_to(edit_user_path(Current.user.id), class: 'btn p-2') do
+          concat(content_tag(:i, ' ', class: 'fas fa-user-edit'))
+        end
+      end)
+      concat(content_tag(:li, class: 'nav-item pe-2') do
+        link_to(user_path(Current.user.id), class: 'btn p-2') do
+          concat(content_tag(:i, ' ', class: 'fas fa-user'))
+        end
+      end)
+      concat(content_tag(:li, class: 'nav-item pe-2') do
+        link_to(logout_path, method: :delete, class: 'btn p-2') do
+          concat(content_tag(:span, 'Log out'))
+          concat(content_tag(:i, ' ', class: 'fas fa-door-open'))
+        end
+      end)
+    end
   end
 
   def right_home
-    if Current.user
-      User.order(created_at: :desc).all.map do |user|
-        content_tag :div, :class=>"mb-3 right_home" do
-          content_tag :div, :class=>"row g-0" do
-            concat(content_tag(:div, class: "col-md-3") do
-                    if user.photo.attached?
-                      tag("img", src: url_for(user.photo), class: ["img-80", "rounded"])
-                    end
-            end)
-            concat(content_tag(:div, class: "col-md-9") do
-              content_tag :div, :class=>"card-body" do
-                concat(content_tag(:div, class: "d-flex justify-content-between mb-2") do
-                  concat(content_tag(:h5, class: "card-title") do
-                    link_to(user_path(user.id), class: "link-dark fw-bold") do
-                      concat(content_tag(:span, user.username))
-                    end
-                  end)
-                  concat(content_tag(:div) do
-                    concat(content_tag(:span, follow_or_unfollow_friend_request(user)))
-                  end)
+    return unless Current.user
+
+    User.order(created_at: :desc).all.map do |user|
+      content_tag :div, class: 'mb-3 right_home' do
+        content_tag :div, class: 'row g-0' do
+          concat(content_tag(:div, class: 'col-md-3') do
+            tag('img', src: url_for(user.photo), class: %w[img-80 rounded]) if user.photo.attached?
+          end)
+          concat(content_tag(:div, class: 'col-md-9') do
+            content_tag :div, class: 'card-body' do
+              concat(content_tag(:div, class: 'd-flex justify-content-between mb-2') do
+                concat(content_tag(:h5, class: 'card-title') do
+                  link_to(user_path(user.id), class: 'link-dark fw-bold') do
+                    concat(content_tag(:span, user.username))
+                  end
                 end)
-                concat(content_tag(:p, class: "card-text") do
-                  concat(content_tag(:small, time_ago_in_words(user.created_at), class: "text-muted"))
-                end)
-              end
-            end)
-          end
+                concat(content_tag(:div) { concat(content_tag(:span, follow_or_unfollow_friend_request(user))) })
+              end)
+              concat(content_tag(:p, class: 'card-text') do
+                concat(content_tag(:small, time_ago_in_words(user.created_at), class: 'text-muted'))
+              end)
+            end
+          end)
         end
-      end.join.html_safe
-    end
+      end
+    end.join.html_safe
   end
 
   def right_user_photo
-    if Current.user.photo.attached?
-      tag("img", src: url_for(@user.photo), class: ["img-100", "rounded-circle", "mx-auto", "d-block"])
-    end
+    return unless Current.user.photo.attached?
+
+    tag('img', src: url_for(@user.photo), class: %w[img-100 rounded-circle mx-auto d-block])
   end
 
   def right_profile(user)
     user.followers.map do |f|
-      content_tag :div, :class=>"mb-1 right_home" do
-        content_tag :div, :class=>"row g-0 p-3" do
-          concat(content_tag(:div, class: "col-md-4") do
-            tag("img", src: url_for(f.follower.photo), class: ["img-80", "rounded-circle", "mx-auto", "d-block"])
+      content_tag :div, class: 'mb-1 right_home' do
+        content_tag :div, class: 'row g-0 p-3' do
+          concat(content_tag(:div, class: 'col-md-4') do
+            tag('img', src: url_for(f.follower.photo), class: %w[img-80 rounded-circle mx-auto d-block])
           end)
-          concat(content_tag(:div, class: "col-md-8") do
-            content_tag :div, :class=>"card-body" do
-              concat(content_tag(:div, class: "card-title fw-bold") do
+          concat(content_tag(:div, class: 'col-md-8') do
+            content_tag :div, class: 'card-body' do
+              concat(content_tag(:div, class: 'card-title fw-bold') do
                 concat(content_tag(:span, f.follower.full_name))
               end)
               concat(content_tag(:span, follow_or_unfollow_friend_request(f.follower)))
