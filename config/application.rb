381b055cb2455a42c6c8480ee9618dae784a1078
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-04-19 17:14:36 +0500

    Fixing issue with deployment

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,9 @@ module Travellers
     config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.serve_static_assets = true
+    config.assets.initialize_on_precompile = false
+    
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
